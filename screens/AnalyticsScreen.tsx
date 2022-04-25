0dd34399a747ee2974fb291efac4d42ba5936aa5
rik <rikrocks4@gmail.com>
2022-04-25 14:08:42 +0530

    Add legends to pie chart

--- screens/AnalyticsScreen.tsx
+++ screens/AnalyticsScreen.tsx
@@ -4,6 +4,7 @@ import {
 	ScrollView,
 	Dimensions,
 	TouchableOpacity,
+	Image,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { VictoryPie } from "victory-native";
@@ -13,10 +14,16 @@ const AnalyticsScreen = () => {
 	const [labelName, setLabelName] = useState("");
 	const defaultGraphicData = [{ y: 0 }, { y: 0 }, { y: 100 }];
 	const wantedGraphicData = [
-		{ x: "Cats", y: 10 },
-		{ x: "Birds", y: 50 },
-		{ x: "Dog", y: 40 },
+		{ x: "Spotify", y: 10, image: require("../assets/spotify-logo.png") },
+		{
+			x: "Prime Video",
+			y: 6,
+			image: require("../assets/prime-video-logo.png"),
+		},
+		{ x: "Netflix", y: 40, image: require("../assets/netflix-logo.png") },
 	];
+
+	const colorScale = ["crimson", "orange", "blue"];
 
 	const [graphicData, setGraphicData] = useState(defaultGraphicData);
 
@@ -25,15 +32,15 @@ const AnalyticsScreen = () => {
 	}, []);
 	return (
 		<ScrollView
-			style={{ backgroundColor: "#20806e", flex: 1, padding: 10 }}>
+			style={{ backgroundColor: "#edeefa", flex: 1, padding: 10 }}>
 			<View
 				style={{
 					marginTop: 10,
 					justifyContent: "center",
 					alignItems: "center",
 					borderRadius: 20,
-					backgroundColor: "#ebebeb",
-					elevation: 8,
+					backgroundColor: "#edeefa",
+
 					padding: 10,
 				}}>
 				<View
@@ -50,7 +57,7 @@ const AnalyticsScreen = () => {
 							color: "#000",
 							fontWeight: "bold",
 						}}>
-						Top 5 Expenses
+						Top Expenses
 					</Text>
 					<Text
 						style={{
@@ -64,45 +71,24 @@ const AnalyticsScreen = () => {
 				</View>
 				<VictoryPie
 					data={graphicData}
+					padAngle={3}
+					cornerRadius={10}
 					animate={{
 						duration: 400,
-						easing: "exp",
+						easing: "circle",
 					}}
 					radius={({ datum }) =>
 						datum.x === labelName ? width * 0.4 : width * 0.4 - 10
 					}
-					innerRadius={70}
-					labelRadius={(width * 0.4 + 70) / 2.5}
-					colorScale={["crimson", "orange", "blue"]}
+					labels={() => null}
+					innerRadius={120}
+					colorScale={colorScale}
 					style={{
-						labels: {
-							fontWeight: "bold",
-							fontSize: 15,
-							fill: "rgba(255,255,255, .9)",
-						},
-						parent: {
-							boxShadow: "4px 4px 4px rgba(0,0,0,.4)",
-						},
+						parent: {},
 					}}
 					height={width * 0.8}
 					width={width * 0.8}
 					events={[
-						{
-							target: "data",
-							eventHandlers: {
-								onPressIn: () => {
-									return [
-										{
-											target: "labels",
-											mutation: (props) => {
-												setLabelName(props.text);
-											},
-										},
-									];
-								},
-							},
-						},
-
 						{
 							target: "parent",
 							eventHandlers: {
@@ -122,64 +108,306 @@ const AnalyticsScreen = () => {
 				<View
 					style={{
 						position: "absolute",
-						left: "42%",
-						top: "48%",
+						left: "35%",
+						top: "45%",
 						justifyContent: "center",
 						alignItems: "center",
 					}}>
 					<Text
 						style={{
-							fontSize: 20,
+							fontSize: 15,
 							fontWeight: "bold",
 							color: "#8d8f8e",
 						}}>
-						Total
+						Total Expenditure
 					</Text>
-					<Text style={{ fontSize: 30, fontWeight: "bold" }}>
-						200 $
+					<Text style={{ fontSize: 50, fontWeight: "bold" }}>
+						$ 200
 					</Text>
 				</View>
 			</View>
-			<View
+			{/* <View
 				style={{
-					backgroundColor: "#ebebeb",
-					marginTop: 10,
-					borderRadius: 20,
-					padding: 10,
-				}}>
-				<View
+					flex: 1,
+					backgroundColor: "blue",
+				}}> */}
+			<ScrollView horizontal showsHorizontalScrollIndicator={false}>
+				{/* <TouchableOpacity
+					onPress={() => setLabelName("Cats")}
 					style={{
-						flexDirection: "row",
-						alignItems: "center",
+						backgroundColor: "orangered",
+						marginTop: 10,
+						borderRadius: 5,
 						padding: 5,
-						marginBottom: 5,
+						elevation: 5,
+						width: width - 50,
+						marginRight: 10,
 					}}>
-					<TouchableOpacity onPress={() => setLabelName("Cats")}>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							padding: 5,
+							marginBottom: 5,
+						}}>
 						<View
 							style={{
-								height: 30,
-								width: 30,
+								height: 15,
+								width: 15,
 								borderRadius: 35,
-								backgroundColor: "crimson",
+								backgroundColor: "#ffff",
 								marginRight: 10,
 							}}></View>
-					</TouchableOpacity>
-					<Text style={{ fontWeight: "bold", fontSize: 15 }}>
-						{" "}
-						Cats{" "}
-					</Text>
-					<View style={{ flexDirection: "row", marginLeft: "auto" }}>
-						<Text style={{ fontWeight: "bold", fontSize: 15 }}>
+
+						<Text
+							style={{
+								fontWeight: "bold",
+								fontSize: 15,
+								color: "#fff",
+							}}>
 							{" "}
-							20 USD
+							Cats{" "}
 						</Text>
-						<Text style={{ fontWeight: "bold", fontSize: 15 }}>
-							{" "}
-							- 11%
-						</Text>
+						<View
+							style={{
+								flexDirection: "row",
+								marginLeft: "auto",
+							}}>
+							<Text
+								style={{
+									fontWeight: "bold",
+									fontSize: 15,
+									color: "#fff",
+								}}>
+								{" "}
+								20 USD
+							</Text>
+							<Text
+								style={{
+									fontWeight: "bold",
+									fontSize: 15,
+									color: "#fff",
+								}}>
+								{" "}
+								- 11%
+							</Text>
+						</View>
 					</View>
-				</View>
-			</View>
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => setLabelName("Dogs")}
+					style={{
+						backgroundColor: "#fff",
+						marginTop: 10,
+						borderRadius: 5,
+						padding: 5,
+						elevation: 5,
+						width: width - 50,
+						marginRight: 10,
+					}}>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							padding: 5,
+							marginBottom: 5,
+						}}>
+						<View
+							style={{
+								height: 15,
+								width: 15,
+								borderRadius: 35,
+								backgroundColor: "blue",
+								marginRight: 10,
+							}}></View>
+
+						<Text
+							style={{
+								fontWeight: "bold",
+								fontSize: 15,
+								color: "#000",
+							}}>
+							{" "}
+							Dogs{" "}
+						</Text>
+						<View
+							style={{
+								flexDirection: "row",
+								marginLeft: "auto",
+							}}>
+							<Text
+								style={{
+									fontWeight: "bold",
+									fontSize: 15,
+									color: "#000",
+								}}>
+								{" "}
+								20 USD
+							</Text>
+							<Text
+								style={{
+									fontWeight: "bold",
+									fontSize: 15,
+									color: "#000",
+								}}>
+								{" "}
+								- 11%
+							</Text>
+						</View>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => setLabelName("Birds")}
+					style={{
+						backgroundColor: "#fff",
+						marginTop: 10,
+						borderRadius: 5,
+						padding: 5,
+						elevation: 5,
+						width: width - 50,
+						marginRight: 10,
+					}}>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							padding: 5,
+							marginBottom: 5,
+						}}>
+						<View
+							style={{
+								height: 15,
+								width: 15,
+								borderRadius: 35,
+								backgroundColor: "yellow",
+								marginRight: 10,
+							}}></View>
+
+						<Text
+							style={{
+								fontWeight: "bold",
+								fontSize: 15,
+								color: "#000",
+							}}>
+							{" "}
+							Birds{" "}
+						</Text>
+						<View
+							style={{
+								flexDirection: "row",
+								marginLeft: "auto",
+							}}>
+							<Text
+								style={{
+									fontWeight: "bold",
+									fontSize: 15,
+									color: "#000",
+								}}>
+								{" "}
+								20 USD
+							</Text>
+							<Text
+								style={{
+									fontWeight: "bold",
+									fontSize: 15,
+									color: "#000",
+								}}>
+								{" "}
+								- 11%
+							</Text>
+						</View>
+					</View>
+				</TouchableOpacity> */}
+				{wantedGraphicData.map((data, ind) => (
+					<TouchableOpacity
+						onPress={() => setLabelName(data.x)}
+						style={{
+							backgroundColor:
+								labelName === data.x
+									? colorScale[ind]
+									: "#ffff",
+							marginTop: 10,
+							borderRadius: 15,
+							overflow: "hidden",
+							padding: 5,
+							elevation: 5,
+							width: 140,
+							height: 140,
+							marginRight: 25,
+							alignItems: "center",
+							justifyContent: "center",
+						}}>
+						<View
+							style={{
+								height: 60,
+								width: 60,
+								borderRadius: 35,
+								backgroundColor: "#Fff",
+								alignItems: "center",
+								justifyContent: "center",
+								marginBottom: 10,
+							}}>
+							<Image
+								style={{ width: 45, height: 45 }}
+								source={data.image}
+							/>
+						</View>
+						<View
+							style={{
+								flexDirection: "row",
+								alignItems: "center",
+
+								marginBottom: 5,
+							}}>
+							<Text
+								style={{
+									fontSize: 14,
+									color:
+										labelName === data.x ? "#FFF" : "#000",
+
+									textTransform: "uppercase",
+								}}>
+								{" "}
+								{data.x}{" "}
+							</Text>
+							<View
+								style={{
+									height: 12,
+									width: 12,
+									borderRadius: 35,
+									backgroundColor: colorScale[ind],
+								}}></View>
+						</View>
+						<View
+							style={{
+								flexDirection: "row",
+							}}>
+							<Text
+								style={{
+									fontWeight: "bold",
+									fontSize: 16,
+									color:
+										labelName === data.x ? "#FFF" : "#000",
+								}}>
+								{" "}
+								$ 20000
+							</Text>
+							<Text
+								style={{
+									fontWeight: "bold",
+									fontSize: 16,
+									color:
+										labelName === data.x ? "#FFF" : "#000",
+								}}>
+								{" "}
+								- 99%
+							</Text>
+						</View>
+					</TouchableOpacity>
+				))}
+				{/*  */}
+			</ScrollView>
+			{/* </View> */}
 		</ScrollView>
 	);
 };
